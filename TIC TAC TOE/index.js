53fb8551c7197bd062e11f293143af4330450ff0
LornaKarungo <168528211+LornaKarungo@users.noreply.github.com>
2024-05-10 23:47:13 +0300

    startGame Function

---
+++ TIC TAC TOE/index.js
@@ -0,0 +1,8 @@
+let startEl = document.getElementById("start-el")
+let container = document.getElementById("container")
+
+function startGame(){
+    startEl.style.display = "none";
+    container.style.display = "block";
+}
+
